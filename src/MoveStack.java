4bf5f636e1bd338f1c95079de6f1eeb868a2adf2
Bhoami K Khona <bhoamikhona@gmail.com>
2025-10-06 06:53:37 -0400

    implemented MoveStack

--- src/MoveStack.java
+++ src/MoveStack.java
@@ -2,8 +2,16 @@
  * Array backed stack for Move objects (LIFO). Implement from scratch
  */
 public class MoveStack {
+
+    // NOTE:
+    // In this design top is the index of the last element, not the count
+    // When stack is empty, top == -1
+    // number of elements = -1 + 1 = 0
+    // after first push, top = 0 so, the size is 0 + 1 = 1
+    // after kth push, top = k-1 so, the size is (k - 1) + 1 = k
+
     private  Move[] data;
-    private int top = -1;
+    private int top = -1; // index of the current element; -1 means empty
 
     public MoveStack(int capacity) {
         this.data = new Move[capacity];
@@ -13,28 +21,57 @@ public class MoveStack {
      * TODO: Implement the pushing of the stack
      * @param m
      */
-    public void push(Move m) {}
+    public void push(Move m) {
+        if (m == null) return;
+
+        if (top + 1 == data.length) {
+            throw new IllegalStateException("MoveStack overflow - capacity: " + data.length);
+        }
+
+        data[++top] = m;
+    }
 
     /** TODO: Uncomment this method and implement the popping of the stack*/
-    // public Move pop() {
-    //     return m;
-    // }
+     public Move pop() {
+
+
+         if (isEmpty()) return null;
+
+         Move m = data[top];
+         data[top] = null;
+         top--;
+
+         return m;
+     }
 
     /**
      * TODO: You need to implement the isEmpty method
      * @return
      */
-    public boolean isEmpty() { return true; }
+    public boolean isEmpty() {
+        return top == -1;
+    }
 
     /**
      * TODO: Implement the clear method
      */
-    public void clear() {}
+    public void clear() {
+
+        // Setting each value of the stack to null
+        for (int i = 0; i <= top; i++) {
+            data[i] = null;
+        }
+
+        // setting the top value to -1
+        top = -1;
+    }
 
 
     /**
      * TODO: You need to implement the size method
      * @return
      */
-    public int size() { return 0;}
+    public int size() {
+        return top + 1;
+    }
 }
